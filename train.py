15239d418b31fe7ae884d44ca04ae983af68b32d
unknown <shashen008he@gmail.com>
2019-06-28 21:58:13 +0800

    update

--- train.py
+++ train.py
@@ -1,3 +1,6 @@
+import matplotlib
+matplotlib.use("Agg")
+
 import os
 import numpy as np
 import torch
@@ -11,6 +14,7 @@ from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 from sklearn.metrics import accuracy_score
 from options import TrainOptions
 import pickle
+from matplotlib import pyplot as plt
 
 def train(log_interval, model, device, train_loader, optimizer, epoch):
     cnn_encoder, rnn_decoder = model
@@ -47,6 +51,48 @@ def train(log_interval, model, device, train_loader, optimizer, epoch):
 
 
         return losses, scores
+
+def validation(model, device, optimizer, test_loader):
+    # set model as testing mode
+    cnn_encoder, rnn_decoder = model
+    cnn_encoder.eval()
+    rnn_decoder.eval()
+
+    test_loss = 0
+    all_y = []
+    all_y_pred = []
+    with torch.no_grad():
+        for X, y in test_loader:
+            # distribute data to device
+            X, y = X.to(device), y.to(device).view(-1, )
+
+            output = rnn_decoder(cnn_encoder(X))
+
+            loss = F.cross_entropy(output, y, reduction='sum')
+            test_loss += loss.item()                 # sum up batch loss
+            y_pred = output.max(1, keepdim=True)[1]  # (y_pred != output) get the index of the max log-probability
+
+            # collect all y and y_pred in all batches
+            all_y.extend(y)
+            all_y_pred.extend(y_pred)
+
+    test_loss /= len(test_loader.dataset)
+
+    # compute accuracy
+    all_y = torch.stack(all_y, dim=0)
+    all_y_pred = torch.stack(all_y_pred, dim=0)
+    test_score = accuracy_score(all_y.cpu().data.squeeze().numpy(), all_y_pred.cpu().data.squeeze().numpy())
+
+    # show information
+    print('\nTest set ({:d} samples): Average loss: {:.4f}, Accuracy: {:.2f}%\n'.format(len(all_y), test_loss, 100* test_score))
+
+    # save Pytorch models of best record
+    torch.save(cnn_encoder.state_dict(), os.path.join(save_model_path, 'cnn_encoder_epoch{}.pth'.format(epoch + 1)))  # save spatial_encoder
+    torch.save(rnn_decoder.state_dict(), os.path.join(save_model_path, 'rnn_decoder_epoch{}.pth'.format(epoch + 1)))  # save motion_encoder
+    torch.save(optimizer.state_dict(), os.path.join(save_model_path, 'optimizer_epoch{}.pth'.format(epoch + 1)))      # save optimizer
+    print("Epoch {} model saved!".format(epoch + 1))
+
+    return test_loss, test_score
 
 
 if __name__ == '__main__':
@@ -157,3 +203,51 @@ if __name__ == '__main__':
 
     optimizer = torch.optim.Adam(crnn_params, lr=learning_rate)
 
+    # record training process
+    epoch_train_losses = []
+    epoch_train_scores = []
+    epoch_test_losses = []
+    epoch_test_scores = []
+
+    # start training    
+    for epoch in range(epochs):
+        # train, test model
+        train_losses, train_scores = train(log_interval, [cnn_encoder, rnn_decoder], device, train_loader, optimizer, epoch)
+        epoch_test_loss, epoch_test_score = validation([cnn_encoder, rnn_decoder], device, optimizer, valid_loader)
+
+        # save results
+        epoch_train_losses.append(train_losses)
+        epoch_train_scores.append(train_scores)
+        epoch_test_losses.append(epoch_test_loss)
+        epoch_test_scores.append(epoch_test_score)
+
+        # save all train test results
+        A = np.array(epoch_train_losses)
+        B = np.array(epoch_train_scores)
+        C = np.array(epoch_test_losses)
+        D = np.array(epoch_test_scores)
+        np.save('./CRNN_epoch_training_losses.npy', A)
+        np.save('./CRNN_epoch_training_scores.npy', B)
+        np.save('./CRNN_epoch_test_loss.npy', C)
+        np.save('./CRNN_epoch_test_score.npy', D)
+
+    # plot
+    fig = plt.figure(figsize=(10, 4))
+    plt.subplot(121)
+    plt.plot(np.arange(1, epochs + 1), A[:, -1])  # train loss (on epoch end)
+    plt.plot(np.arange(1, epochs + 1), C)         #  test loss (on epoch end)
+    plt.title("model loss")
+    plt.xlabel('epochs')
+    plt.ylabel('loss')
+    plt.legend(['train', 'test'], loc="upper left")
+    # 2nd figure
+    plt.subplot(122)
+    plt.plot(np.arange(1, epochs + 1), B[:, -1])  # train accuracy (on epoch end)
+    plt.plot(np.arange(1, epochs + 1), D)         #  test accuracy (on epoch end)
+    plt.title("training scores")
+    plt.xlabel('epochs')
+    plt.ylabel('accuracy')
+    plt.legend(['train', 'test'], loc="upper left")
+    title = "./fig_UCF101_ResNetCRNN.png"
+    plt.savefig(title, dpi=600)
+    plt.close(fig)
